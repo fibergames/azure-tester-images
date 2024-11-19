ddec2aeb611a32119718c6844083d0f72d6488e1
Fibergames <warmachin3@gmail.com>
2024-11-19 19:04:42 +0100

    555

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
+const cors = require("cors");
 const express = require("express");
 const app = express();
+
+app.use(cors());
 
 app.get("/api/message", (req, res) => {
     res.json({ message: "Hello from Node.js Backend!" });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
